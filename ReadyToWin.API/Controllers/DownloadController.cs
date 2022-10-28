4446699a507099daa22c7468fb3866de252df1cf
Abdul <abdulrub44469@gmail.com>
2022-10-28 23:29:47 +0530

    one more item

---
+++ ReadyToWin.API/Controllers/DownloadController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReadyToWin.API.Controllers
+{
+    [RoutePrefix("Download")]
+    public class DownloadController : Controller
+    {
+        // GET: Download
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("Index")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public FileResult apk(string filename)
+        {
+            var FileVirtualPath = "~/App_Data/"+ filename;
+            return File(FileVirtualPath, "application/force-download", Path.GetFileName(FileVirtualPath));
+        }
+        
+    }
+}
